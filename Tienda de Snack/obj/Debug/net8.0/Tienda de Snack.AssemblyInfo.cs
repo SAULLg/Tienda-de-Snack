b78d85137843beb5f973525e0d46dcc9a84e481f
SoniaOC <sonia.ontiveros@uabc.edu.mx>
2025-10-05 02:27:36 -0700

    .gitignore

--- Tienda de Snack/obj/Debug/net8.0/Tienda de Snack.AssemblyInfo.cs
+++ Tienda de Snack/obj/Debug/net8.0/Tienda de Snack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tienda de Snack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d85ed8ce904922addea55d0cf08186fa1cd32bec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tienda de Snack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tienda de Snack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
